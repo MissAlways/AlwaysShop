4e714d9c8b6a377821f3dfedbe441261ed4f56a0
Aina <>
2022-10-31 08:23:15 +0200

    Get product images by product id. Fixed #6

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,20 @@ app.get('/products', async (req, res) => {
     }
 });
 
+// Get product images by product id
+app.get("/products/images/:id", async (req, res) => {
+    try {
+        let id = parseInt(req.params.id);
+        const result = await db.pool.query(
+            "SELECT url " +
+            "FROM product_image " +
+            "WHERE product_id = ?",
+            [id]
+            );
+            return res.status(200).json(result);
+        } catch (error) {
+        return res.status(503).json({ message: "Database currently not available. Error: " + error})
+    }
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
